9cf6d8721760f2734628b1c431687c204353fccc
yuran1811 <trieuvanbd123@gmail.com>
2024-12-20 16:57:55 +0700

    feat: ✨ refactor payment gateway registration to use PAYMENT_METHODS enum for better maintainability

--- src/ecommerce/src/core/Payment.cpp
+++ src/ecommerce/src/core/Payment.cpp
@@ -3,8 +3,12 @@
 void registerPaymentGateways() {
   PaymentGatewayRegistry& registry = PaymentGatewayRegistry::getInstance();
 
-  registry.registerFactory("credit", make_unique<CreditCardPaymentFactory>());
-  registry.registerFactory("paypal", make_unique<PayPalPaymentFactory>());
-  registry.registerFactory("stripe", make_unique<StripePaymentFactory>());
-  registry.registerFactory("cod", make_unique<CODPaymentFactory>());
+  registry.registerFactory(PAYMENT_METHODS[PaymentMethod::CREDIT_CARD],
+                           make_unique<CreditCardPaymentFactory>());
+  registry.registerFactory(PAYMENT_METHODS[PaymentMethod::PAYPAL],
+                           make_unique<PayPalPaymentFactory>());
+  registry.registerFactory(PAYMENT_METHODS[PaymentMethod::STRIPE],
+                           make_unique<StripePaymentFactory>());
+  registry.registerFactory(PAYMENT_METHODS[PaymentMethod::COD],
+                           make_unique<CODPaymentFactory>());
 }
